9a363cd5fd480a99a381653e804ea153d33eef85
skididdles <mattchenry@hotmail.com>
2014-10-03 17:13:03 -0400

    studentstruct creates struct for tracking students

--- practice.hs
+++ practice.hs
@@ -13,4 +13,11 @@ splitList b a
 eminem :: (Integral a) => [[a]] -> [[a]]
 eminem xxs = [ [ x | x <- xs, even x ] | xs <- xxs]  ++ [ [ y | y <- ys, odd y ] | ys <- xxs]
 
+--Checks if the total number of students can be split into said group size
 checkmod x y = (if mod x y == 0 then putStrLn "Yay" else putStrLn $"Cannot pair up " ++ (show x) ++ " students into groups of " ++ show(y) ++ "across eight assignments")
+
+--Takes list of total students [1..howevermany]
+--Out puts a list of tuples first element being the student ID number
+--Second being an empty list 
+--for storing the students already been grouped with
+studentstruct ss = [(x,[]) | x <- ls]
