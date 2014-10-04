3fcdb43cde3957996930f2919159b0fc9284f855
Matt Henry <eldushbago@gmail.com>
2014-10-03 21:43:35 -0400

    started addstudent

--- practice.hs
+++ practice.hs
@@ -22,11 +22,17 @@ checkmod x y = (if mod x y == 0 then putStrLn "Yay" else putStrLn $"Cannot pair 
 --for storing the students already been grouped with
 studentstruct ss = [(x,[x]) | x <- ss]
 
---Takes the studentstruct
-checkstudentused ss = map snd ss
+--right now adds student to everyone 
+addstudent ss s = [(a,s:[b])|(a,[b]) <- ss]
+--checkstudentused ss i = map elem i (map snd ss)
+chch ss = map snd ss 
 --	| ss == 1 "YAY"
 --	| length $ map snd ss > 1 = "something else"
 --	| otherwise	= "WHAT!?"
+--checkindivdual :: Num => Num i -> (Num,[Num]) -> IO
+checkindivdual i t= if elem i $ snd t 
+					then putStrLn "yay"
+					else putStrLn "boo"
 
 
 
