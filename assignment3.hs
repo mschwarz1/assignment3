433b959b1c6bd86a66b5c9d09b5517afe16b0a20
sdwoodbury <yr45570@umbc.edu>
2014-10-03 22:59:30 -0400

    this gets two arguments but i still need to figure out how the hell to pass them to a pure function

--- assignment3.hs
+++ assignment3.hs
@@ -1,11 +1,17 @@
 import System.Environment
 import Data.List
+import System.IO
 
+--http://www.reddit.com/r/haskell/comments/1vras3/haskell_io_how_to_read_numbers/
 
 main = do
 	args <- getArgs
 	progName <- getProgName
-	putStrLn "The arguments are:"
-	mapM putStrLn args
+	putStrLn "The number of stoodents aer:"
+        putStrLn $head args
+        putStrLn "Teh greup siez is: "
+        putStrLn $head $tail args
+        --mapM putStrLn args
 	putStrLn "Program name is:"
 	putStrLn progName
+        
