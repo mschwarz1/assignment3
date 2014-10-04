69b8a79d80aa73b7aa3ac82f5b7017e9ada34969
sdwoodbury <yr45570@umbc.edu>
2014-10-04 00:09:08 -0400

    This looks more professional

--- assignment3.hs
+++ assignment3.hs
@@ -3,15 +3,25 @@ import System.IO
 import Data.List
 import System.IO
 
+
 --http://www.reddit.com/r/haskell/comments/1vras3/haskell_io_how_to_read_numbers/
 
-main = do
+
+main :: IO ()
+main = 
+     do
 	args <- getArgs
 	progName <- getProgName
+        let students = (head args)
+        let grpsize = head $tail args
 	putStrLn "The number of stoodents aer:"
-        putStrLn $head args
+        putStrLn students
         putStrLn "Teh greup siez is: "
-        putStrLn $head $tail args
+        putStrLn grpsize
 	putStrLn "Program name is:"
 	putStrLn progName
-        
+        return (students, grpsize)        
+        return ()
+
+
+
