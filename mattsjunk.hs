7b63d8c2be68691873d9d1aff12f93603340a0ec
Matt Henry <eldushbago@gmail.com>
2014-10-05 19:51:17 -0400

    started update group tweaking checkindividual

--- mattsjunk.hs
+++ mattsjunk.hs
@@ -1,22 +1,46 @@
---Takes list of total students [1..howevermany]
---Out puts a list of tuples first element being the student ID number
---Second being a list
---for storing the students already been grouped with
+{-
+	Takes list of total students [1..howevermany]
+	Outputs a list of tuples first element being the student ID number
+	Second being a list
+	for storing the students already been grouped with
+-}
 studentstruct ss = [(x,[x]) | x <- ss]
 
---s = the student to add
---curstu = the tuple  where first element is the student id
---and the second element is the list of already grouped students
+{-
+	s = the student to add
+	curstu = the tuple  where first element is the student id
+	and the second element is the list of already grouped students
+-}
 addstudent s curstu = head [ (y, s: x) | let x = snd curstu , let y = fst curstu]
 
 
--- Return types being different is making it difficult
--- studentstruct !! number gives you currentstudent looking for is the student to check for
+--TODO get type signature right
+--updategroupedlist :: (Int x Int [y]) => x -> [y] -> [z]
+--updategroupedlist n l = filter (not . (==n)) [l] 
+
+{-
+	studentstruct !! number gives you currentstudent 
+	looking for is the student to check for
+	returns true if student has already been grouped with this student
+	otherwise will add student to group
+	TODO needs to the update grouped already list
+-}
 checkindivdual lookingfor currentstudent = 
 	case elem lookingfor $ snd currentstudent of
-		True -> Right "Already there"
+		True -> Right True
 		False -> Left $ addstudent lookingfor currentstudent
 
+{-
+altcheckind lookingfor currentstudent = 
+	(if elem lookingfor $ snd currentstudent
+		then do {
+				return
+				}
+		else
+			do { 
+				addstudent lookingfor currentstudent
+				}
+-}
 
 
 --Checks if the total number of students can be split into said group size
@@ -24,13 +48,12 @@ checkmod x y = (if mod x y == 0
 						then putStrLn "Yay" 
 						else putStrLn $"Cannot pair up " ++ 
 						(show x) ++ " students into groups of " 
-						++ show(y) ++ "across eight assignments")
-
---should iterate entire list of students
---call checkindividual lfor = student to look for
---x is just the size of list (should be a way to hack it to use "length students")
---Error about show IO cause it prints yay if finds it instead of taking real action
---too lazy right now to figure it out
---checkall students lfor x= [checkindivdual lfor (students !! index )| index <- [1..x]]
-
-
+						++ show(y) ++ " across eight assignments")
+{-
+	should iterate entire list of students
+	call checkindividual lfor = student to look for
+	x is just the size of list (should be a way to hack it to use "length students")
+	Error about show IO cause it prints yay if finds it instead of taking real action
+	too lazy right now to figure it out
+-}
+checkall students lfor x= [checkindivdual lfor (students !! index )| index <- [1..x]]
