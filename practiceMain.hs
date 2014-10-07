c9dd466e3d6ab9ec2f68557ce06f9ac80a2cde9d
sdwoodbury <yr45570@umbc.edu>
2014-10-07 09:03:05 -0400

    starting to play with ways to handle the i/o

--- practiceMain.hs
+++ practiceMain.hs
@@ -3,16 +3,31 @@ import System.Environment
 import System.IO
 import Data.List
 import System.IO
+import Data.List.Split
 
-
+----------------------------------------------------------------------------------
 --TODO make output True for true conidtion
 --Checks if the total number of students can be split into said group size
 checkmod :: Int -> Int -> Bool
 checkmod x y 
        | mod x y == 0 = True
        | otherwise = False
+-----------------------------------------------------------------------------------
+--split list makes a bunch of sublists
+splitList :: Int -> Int -> [[Int]]
+splitList b a 
+     | a == 0 = []
+     | otherwise = chunksOf a [1..b]
 
-
+----------------------------------------------------------------------------
+--takes [[final list of student groups]] [student list] group size
+--listGen perhaps will build a list of groups, determine if it can continue or not and if 
+--the conditions have been met, and then print out the output 
+listGen :: [[Int]] -> [Int] -> Int -> IO ()
+listGen endList students groupSize
+       | (length endList) == ((length students) `div` groupSize) * 8 = putStrLn "Success"
+       | otherwise = putStrLn "Can not group everyone up in groups that size 8 times"
+----------------------------------------------------------------------------------
 
 --http://www.reddit.com/r/haskell/comments/1vras3/haskell_io_how_to_read_numbers/
 
@@ -30,17 +45,12 @@ main =
         let grpsize = read(head $tail args) :: Int
 
         if checkmod students grpsize
-           then	
-               do
-                 putStrLn "The number of stoodents aer:"
-                 putStrLn $show students
-                 putStrLn "Teh greup siez is: "
-                 putStrLn $show grpsize
-	         putStrLn "Program name is:"
-	         putStrLn progName            
+           then
+               listGen (splitList students grpsize) [1..students] grpsize
+                 
            else
                do
-                 putStrLn "Fuck You"
+                 putStrLn "Can not group everyone up in groups that size"
    
         
 
